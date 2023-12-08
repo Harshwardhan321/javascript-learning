e12e2c115b4edc7d0725a95723d4341a700cf1c6
Harshwardhan <harshwardhan321@gmail.com>
2023-12-08 19:47:40 +0530

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // Treats all JS code as newer version
+
+// alert(40+2);
+//we are using node js not browser.
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("harsh")
+
+
+let name = "harsh"
+let age = 24
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value - when no any value is returned or empty value returned this is called null
+// undefined => value has been not defined
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
