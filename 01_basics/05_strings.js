17db7cb7bfffd4756083986a0bc84312ef04b695
Harshwardhan <55048959+Harshwardhan321@users.noreply.github.com>
2023-12-12 13:53:36 +0000

    --

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,7 +1,7 @@
 const name = "harsh";
 const repoCount = 50;
 
-// console.log(name + repoCount + " Value");
+console.log(name + repoCount + " Value");
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
