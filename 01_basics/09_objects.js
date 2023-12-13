3bb4864ffbc9794892b8dff5b3f6c3eb15cc936d
Harshwardhan <55048959+Harshwardhan321@users.noreply.github.com>
2023-12-13 15:44:14 +0000

    symbol declaration

--- 01_basics/09_objects.js
+++ 01_basics/09_objects.js
@@ -5,9 +5,16 @@
 // In object, Parameters are assigned as key-value pairs
 // By default system processes keys as strings
 
+// Symbol Declaration
+
+const sym1 = Symbol("key1")
+// to use symbol in object the syntax is : [sym1]: mykey1 (Any desired value)
+
 // Creating a Object :-
 const User = {
     name: "Harsh",
+    "Full Name": "Harshwardhan",
+    [sym1]: "mykey1",
     age: 24,
     location: "Patna",
     email: "harshwardhan321@gmail.com",
@@ -16,7 +23,12 @@ const User = {
 }
 
 // Accessing a Object
-// Two ways to access objects and both are important
+// Two method to access objects and both are important
+// To access full name from the object we have to use 2nd method to acess an object because there is no way we can access this in first method.
 
-console.log(User.email);
-console.log(User[email]);
+console.log(User.email); // Method 1
+console.log(User["Full Name"]); // Method 2
+
+// To access symbol we have to use second method
+
+console.log(User[sym1]);
