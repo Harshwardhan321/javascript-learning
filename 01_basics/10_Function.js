9aef2e3c3a7c5ae6e31e3596089d4ba5044e7595
Harshwardhan <55048959+Harshwardhan321@users.noreply.github.com>
2023-12-15 15:21:28 +0000

    passing object in fun

--- 01_basics/10_Function.js
+++ 01_basics/10_Function.js
@@ -34,4 +34,38 @@ function loginUserMessage(username = "sam"){
 }
 
 console.log(loginUserMessage("harsh"))
-console.log(loginUserMessage("harsh"))
+
+// Cart example
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+// Another case - It return the first two values in val1 and val2 and remaining in array.
+// function calculateCartPrice(val1, val2, ...num1){
+//     return num1
+// }
+
+
+console.log(calculateCartPrice(2, 20, 200, 2000));
+
+// Rest Operator - While we pass parameter we can use (...) which store all values inside array
+// Output - [ 2, 20, 200, 2000 ]
+
+// Object Passing in function
+
+const user = {
+    user1: "Harsh",
+    price: 209
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.user1} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+// we can also directly pass object
+handleObject({
+    username1: "sam",
+    price: 399
+})
