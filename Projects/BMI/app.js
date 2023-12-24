4c2469c37201b1e68fe84906e560b0fd3c6d62ad
Harshwardhan <harshwardhan321@gmail.com>
2023-12-24 16:50:36 +0530

    minor changes

--- Projects/BMI/app.js
+++ Projects/BMI/app.js
@@ -16,8 +16,8 @@ form.addEventListener('submit', function(event) {
     }
     else {
         const bmi = (weight / ((height*height) / 10000)).toFixed(2)
-
+        // show the result
         results.innerHTML = `<span> ${bmi}</span>`;
         
     }
-})
+});
