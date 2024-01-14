5553b066a74b40683dee9f7f7d99773bb9943053
Harshwardhan <harshwardhan321@gmail.com>
2024-01-14 20:42:38 +0530

    project

--- Projects/Unlimited colors/app.js
+++ Projects/Unlimited colors/app.js
@@ -14,17 +14,26 @@ let intervalId;
 
 const startChangingColor = function () {
     // Start changing color every 1000 milliseconds (1 second)
-    intervalId = setInterval(changeColor, 1000);
-
-    function changeColor() {
-        document.body.style.backgroundColor = randomColor();
+    if(!intervalId) {
+        intervalId = setInterval(changeColor, 1000);
     }
+
+    const newColor = randomColor();
+    function changeColor() {
+        document.body.style.backgroundColor = newColor;
+        // document.getElementById('hex-display').innerText = newColor;
+    }
+     // Update the hex color display
+     
 };
 
 const stopChangingColor = function () {
     // Clear the interval to stop changing color
     clearInterval(intervalId);
+    intervalId = null;
 };
 
 document.querySelector('#start').addEventListener('click', startChangingColor);
 document.querySelector('#stop').addEventListener('click', stopChangingColor);
+
+
